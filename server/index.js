8bf2ecd4552798c1820690d29632e54c0f0cc13e
PortalFan202 <anibaldonoso1@gmail.com>
2022-10-04 10:58:02 +0300

    Installed mysql2

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const port = 3001
+
+app.listen(port)
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World!' })
+})
