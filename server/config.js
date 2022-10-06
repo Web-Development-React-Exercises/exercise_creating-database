6edf57e1229ea444279d84993eea99513286d208
PortalFan202 <anibaldonoso1@gmail.com>
2022-10-06 08:39:48 +0300

    fixed config file and update gitignore

--- server/config.js
+++ server/config.js
@@ -1,9 +1,12 @@
 const config = {
-    host: "localhost",
-    user: 'root', 
-    port: 3306,
-    password: '',
-    database: "todo"
+    db: {
+        host: "localhost",
+        user: 'root',
+        port: 3306,
+        password: '',
+        database: "todo"
+    }
+
 }
 
 module.exports = config
