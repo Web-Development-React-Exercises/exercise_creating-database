713be576909c3e34808b797e8879e83eff1ef012
PortalFan202 <anibaldonoso1@gmail.com>
2022-10-13 10:04:55 +0300

    Added UI to add and delete tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import axios from 'axios';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
 
   const URL = 'http://localhost:3001'
 
   useEffect(() => {
+
     axios.get(URL)
       .then((response) => {
         setData(response.data);
@@ -18,12 +21,46 @@ function App() {
       );
   }, [])
 
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({ description: newTask })
+    axios.post(URL + '/new', json, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((response) => {
+        const addedObject = JSON.parse(json);
+        addedObject.id = response.data.id;
+        setData(data => [...data, addedObject]);
+        setNewTask('');
+      })
+      .catch((response) => {
+        alert('Error: ' + response.code + '\n' + response.message);
+      });
+  }
+
+  function remove(id) {
+    axios.delete(URL + '/delete' + '?id=' + id)
+      .then(()=>{
+        const newTaskWithoutRemoved = data.filter((task) => task.id !== id);
+        setData(newTaskWithoutRemoved);
+      })
+      .catch (error => {
+        alert('Error: ' + error.code + '\n' + error.message);
+      })
+  }
+
 
   return (
     <>
+      <h3>Tasks</h3>
+      <form onSubmit={save}>
+        <label htmlFor="">Add Task</label>
+        <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
+        <button>Submit</button>
+      </form>
       <ul>
         {data.map((item) => (
-          <li key={item.id}>{item.description}</li>
+          <li key={item.id}>{item.description} <a href="#" onClick={()=>remove(item.id)}>Remove</a> </li>
         ))}
       </ul>
     </>
