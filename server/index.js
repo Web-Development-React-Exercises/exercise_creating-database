5ab7036d01f7df0576a811feb40639eb84032d6b
PortalFan202 <anibaldonoso1@gmail.com>
2022-10-13 10:20:47 +0300

    Finished part 5

--- server/index.js
+++ server/index.js
@@ -1,48 +1,21 @@
 const express = require('express')
 const cors = require('cors')
-const mysql = require('mysql2/promise')
-const config = require('./config')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+const todoRouter = require('./routes/todo.js')
+
 const port = 3001
 
-app.get('/', async (req, res) => {
-    try {
-        const conn = await mysql.createConnection(config.db)
-        const [result,] = await conn.execute('SELECT * FROM task')
-        if (!result) result = [];
-        res.status(200).json(result)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ error: err.message })
-    }
-})
+app.use('/', todoRouter)
 
-app.post('/new', async (req, res) => {
-    try {
-        const conn = await mysql.createConnection(config.db)
-        const [result,] = await conn.execute('insert into task (description) values (?)',[req.body.description])
-        res.status(200).json({id:result.insertId})
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ error: err.message })
-    }
-})
-
-app.delete('/delete', async (req, res) => {
-    try {
-        const conn = await mysql.createConnection(config.db)
-        await conn.execute('delete from task where id = ?', [req.query.id])
-        res.status(200).json({id:req.query.id})
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ error: err.message })
-    }
+app.use((err,req,res,next)=>{
+    const statusCode = res.statusCode || 500
+    console.error(err.message,err.stack)
+    res.status(statusCode).json({error:err.message})
 })
 
 app.listen(port)
