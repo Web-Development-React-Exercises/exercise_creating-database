5f09ab4dd834b47aeae08c8027fb9d828ea4cd55
PortalFan202 <anibaldonoso1@gmail.com>
2022-10-20 10:54:16 +0300

    Finished exercise

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -6,26 +6,80 @@ const newTask = {
     description: "My test task"
 }
 
-describe('GET /', ()=>{
+describe('GET /', () => {
     let task = null
 
-    beforeAll(async ()=>{
+    beforeAll(async () => {
         const response = await request(URL).post('/new').send(newTask)
         task = response.body
     })
 
-    afterAll(async()=>{
+    afterAll(async () => {
         await request(URL).delete(`/delete?id=${task.id}`)
     })
 
-    test('Return 200', async()=>{
+    test('Return 200', async () => {
         const response = await request(URL).get('/')
-        expect(response.statusCode).toBe(200)
+
     })
 
-    test('Should return todos', async()=>{
+    test('Should return todos', async () => {
         const response = await request(URL).get('/')
+        expect(response.statusCode).toBe(200)
         expect(response.body.length >= 1).toBe(true)
     })
 })
 
+describe('POST/', () => {
+    let task = null
+
+    afterAll(async () => {
+        await request(URL).delete(`/delete?id=${task.id}`)
+    })
+
+    test('Add new task', async () => {
+        const response = await request(URL).post('/new').send(newTask)
+        task = response.body
+        expect(response.statusCode).toBe(200)
+        expect(task.id).not.toBe(null)
+    })
+})
+
+describe('DELETE/', () => {
+    let task = null
+
+    beforeAll(async () => {
+        const response = await request(URL).post('/new').send(newTask)
+        task = response.body
+    })
+
+    test('Delete task', async () => {
+        const response = await request(URL).delete(`/delete?id=${task.id}`)
+        expect(response.statusCode).toBe(200)
+        expect(Number(response.body.id)).toBe(Number(task.id))
+    })
+})
+
+describe('PUT/', () => {
+    let task = null
+
+    beforeAll(async () => {
+        const response = await request(URL).post('/new').send(newTask)
+        task = response.body
+    })
+
+    afterAll(async () => {
+        await request(URL).delete(`/delete?id=${task.id}`)
+    })
+
+    test('Edit a task', async () => {
+        const editedTask = {
+            id: task.id,
+            description: 'My edited task'
+        }
+
+        const response = await request(URL).put('/edit').send(editedTask)
+        expect(response.statusCode).toBe(200)
+        expect(Number(response.body.affectedRows)).toBe(1)
+    })
+})
